9f12de53dd5c990c39cd9bc69e78858034673363
Shane Power <20102592@mail.wit.ie>
2025-10-07 12:22:42 +0100

    Create AddMoodActivity, implement basic logging using Timber

--- app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
+++ app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ie.setu.moodjournal.R
+import timber.log.Timber
+import timber.log.Timber.i
 
-class MainActivity : AppCompatActivity() {
+class AddMoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Timber.plant(Timber.DebugTree())
+        i("Add Mood activity started")
     }
+
 }
