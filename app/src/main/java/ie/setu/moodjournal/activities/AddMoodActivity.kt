151b2fae14db6e9d0e6790970b6f2b3ba9fd91ac
Shane Power <20102592@mail.wit.ie>
2025-10-17 14:24:39 +0100

    Add button binding

--- app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
+++ app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
@@ -1,20 +1,25 @@
 package ie.setu.moodjournal.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ie.setu.moodjournal.R
+import ie.setu.moodjournal.databinding.ActivityAddmoodentryBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class AddMoodActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddmoodentryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityAddmoodentryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
         i("Add Mood activity started")
+
+        binding.btnAdd.setOnClickListener {
+            i("Add Button pressed")
+        }
     }
 
 }
