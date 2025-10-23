a2e16436136ab80cb3a369306c0ae0bcfc19e211
Shane Power <20102592@mail.wit.ie>
2025-10-23 20:41:07 +0100

    Add Mood color picker functionality, remove unnecessary "id" var in mood model

--- app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
+++ app/src/main/java/ie/setu/moodjournal/activities/AddMoodActivity.kt
@@ -4,6 +4,9 @@ import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.view.View
+import ie.setu.moodjournal.R
 import ie.setu.moodjournal.databinding.ActivityAddmoodentryBinding
 import ie.setu.moodjournal.main.MainApp
 import ie.setu.moodjournal.models.MoodEntryModel
@@ -15,6 +18,8 @@ class AddMoodActivity : AppCompatActivity() {
     private lateinit var app : MainApp
     var moodEntry = MoodEntryModel()
     private var selectedDate: LocalDate = LocalDate.now()
+    private var selectedColor: Int = 0
+    private var selectedLabel: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,21 +34,64 @@ class AddMoodActivity : AppCompatActivity() {
         binding.dateText.setOnClickListener {
             showDatePicker()
         }
-
+        setMoodColorListeners()
 
         binding.btnAdd.setOnClickListener {
             moodEntry.notes = binding.moodNotes.text.toString()
-            moodEntry.moodColor = 0
+            moodEntry.moodColor = selectedColor
+            moodEntry.moodLabel = selectedLabel
             moodEntry.date = selectedDate
-            app.moodEntries.add(moodEntry.copy())
-
-            i("add Button Pressed: ${moodEntry}")
+            if (moodEntry.moodColor != 0) {
+                app.moodEntries.add(moodEntry.copy())
+                i("add Button Pressed: ${moodEntry}")
+                for (i in app.moodEntries.indices) {
+                    i("Mood [$i]:${this.app.moodEntries[i]}")
+                }
+            }
         }
-
 
     }
 
+    private fun setMoodColorListeners() {
+        val moodViews = listOf(
+            binding.colorAwful,
+            binding.colorBad,
+            binding.colorNeutral,
+            binding.colorGood,
+            binding.colorGreat
+        )
 
+        val moodColors = listOf(
+            R.color.awful,
+            R.color.bad,
+            R.color.neutral,
+            R.color.good,
+            R.color.great
+        )
+
+        val moodLabels = listOf(
+            "Awful",
+            "Bad",
+            "Neutral",
+            "Good",
+            "Great"
+        )
+
+        moodViews.forEachIndexed { index, view ->
+            view.setOnClickListener {
+                selectedColor = ContextCompat.getColor(this, moodColors[index])
+                selectedLabel = moodLabels[index]
+
+                highlightSelectedMood(view, moodViews)
+                i("Mood selected: $selectedLabel with color $selectedColor")
+            }
+        }
+    }
+
+    private fun highlightSelectedMood(selectedView: View, allViews: List<View>) {
+        allViews.forEach { it.alpha = 0.4f } // dim all
+        selectedView.alpha = 1.0f // highlight selected
+    }
     private fun showDatePicker() {
         val calendar = Calendar.getInstance()
         val datePicker = DatePickerDialog(
--- app/src/main/java/ie/setu/moodjournal/models/MoodEntryModel.kt
+++ app/src/main/java/ie/setu/moodjournal/models/MoodEntryModel.kt
@@ -3,8 +3,8 @@ package ie.setu.moodjournal.models
 import java.time.LocalDate
 
 data class MoodEntryModel(
-    var id: Long = 0,
     var moodColor: Int = 0,
+    var moodLabel: String = "",
     var notes: String = "",
     var date: LocalDate = LocalDate.now())
 
