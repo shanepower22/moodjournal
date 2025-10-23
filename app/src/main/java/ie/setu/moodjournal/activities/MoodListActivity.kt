5aa68b4854c5538693a4739dfd76eae7ad2d6db7
Shane Power <20102592@mail.wit.ie>
2025-10-23 22:55:08 +0100

    Mood list activity implementation with placeholder moods

--- app/src/main/java/ie/setu/moodjournal/activities/MoodListActivity.kt
+++ app/src/main/java/ie/setu/moodjournal/activities/MoodListActivity.kt
@@ -1,20 +1,66 @@
 package ie.setu.moodjournal.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ie.setu.moodjournal.R
 import ie.setu.moodjournal.main.MainApp
+import ie.setu.moodjournal.databinding.ActivityMoodListBinding
+import ie.setu.moodjournal.databinding.CardMoodBinding
+import ie.setu.moodjournal.models.MoodEntryModel
 
 class MoodListActivity : AppCompatActivity() {
 
-    private lateinit var app : MainApp
+
+    private lateinit var app: MainApp
+
+    private lateinit var binding: ActivityMoodListBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mood_list)
+        binding = ActivityMoodListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         app = application as MainApp
-        }
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = MoodAdapter(app.moodEntries)
     }
+
+
+}
+class MoodAdapter (private var moodEntries: List<MoodEntryModel>)
+    : RecyclerView.Adapter<MoodAdapter.MainHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardMoodBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val mood = moodEntries[holder.adapterPosition]
+        holder.bind(mood)
+    }
+
+    override fun getItemCount(): Int = moodEntries.size
+
+    class MainHolder(private val binding: CardMoodBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(mood: MoodEntryModel) {
+            binding.moodLabel.text = binding.root.context.getString(
+                R.string.moodColorLabel,
+                mood.moodLabel
+            )
+            binding.moodColorIndicator.setBackgroundColor(mood.moodColor)
+            binding.moodNotes.text = mood.notes
+            binding.moodDate.text = mood.date.toString()
+        }
+            }
+}
--- app/src/main/java/ie/setu/moodjournal/main/MainApp.kt
+++ app/src/main/java/ie/setu/moodjournal/main/MainApp.kt
@@ -1,9 +1,12 @@
 package ie.setu.moodjournal.main
 
 import android.app.Application
+import android.graphics.Color
 import ie.setu.moodjournal.models.MoodEntryModel
 import timber.log.Timber
 import timber.log.Timber.i
+import java.time.LocalDate
+import androidx.core.graphics.toColorInt
 
 class MainApp : Application() {
     val moodEntries = ArrayList<MoodEntryModel>()
@@ -12,5 +15,8 @@ class MainApp : Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("Mood Entry App started")
+        moodEntries.add(MoodEntryModel("#2E7D32".toColorInt(), "great", "Passed exam", LocalDate.now()))
+        moodEntries.add(MoodEntryModel("#C62828".toColorInt(), "awful", "Car crash", LocalDate.of(2025, 10,20)))
+
     }
 }
