368f535d148f310144f38fae0d0397639c64309e
Shane Power <20102592@mail.wit.ie>
2025-10-25 14:45:36 +0100

    Implement Parcelize on model

--- app/src/main/java/ie/setu/moodjournal/models/MoodEntryModel.kt
+++ app/src/main/java/ie/setu/moodjournal/models/MoodEntryModel.kt
@@ -1,10 +1,13 @@
 package ie.setu.moodjournal.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
+@Parcelize
 data class MoodEntryModel(
     var moodColor: Int = 0,
     var moodLabel: String = "",
     var notes: String = "",
-    var date: LocalDate = LocalDate.now())
+    var date: LocalDate = LocalDate.now()) : Parcelable
 
