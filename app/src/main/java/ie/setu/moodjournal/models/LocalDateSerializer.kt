06a7df622e41d7185ad12595df7cc57d85fb8b8a
Shane Power <20102592@mail.wit.ie>
2025-10-25 17:34:44 +0100

    Create Local Date serializer as serializer does not support LocalDate type

---
+++ app/src/main/java/ie/setu/moodjournal/models/LocalDateSerializer.kt
@@ -0,0 +1,22 @@
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+//source: https://slack-chats.kotlinlang.org/t/461582/hello-i-m-trying-to-deserialize-into-a-localdate-object-from
+object LocalDateSerializer : KSerializer<LocalDate> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        val result = value.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        encoder.encodeString(result)
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString())
+    }
+}
