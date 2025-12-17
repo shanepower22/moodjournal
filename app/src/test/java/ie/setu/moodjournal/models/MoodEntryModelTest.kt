16b12fb3f02898f7741968265aad2d02421ffcf7
Shane Power <20102592@mail.wit.ie>
2025-12-17 23:06:03 +0000

    Add unit tests, fix ui bugs

--- app/src/test/java/ie/setu/moodjournal/models/MoodEntryModelTest.kt
+++ app/src/test/java/ie/setu/moodjournal/models/MoodEntryModelTest.kt
@@ -2,39 +2,84 @@ package ie.setu.moodjournal.models
 
 import org.junit.Assert.*
 import org.junit.Test
-import java.time.LocalDate
 
 class MoodEntryModelTest {
 
     @Test
-    fun validMoodEntry_PassesValidation() {
+    fun validMoodEntry_ReturnsTrue() {
         val mood = MoodEntryModel(
-            moodColor = 123456,
+            id = 1,
+            moodColor = 0xFF00FF,
             moodLabel = "Happy",
-            notes = "Had a great day",
-            date = LocalDate.now()
+            notes = "Good day",
+            lat = 52.245,
+            lng = -7.139,
+            zoom = 15f,
+            imageUri = null,
+            date = "2025-03-10"
         )
+
         assertTrue(mood.validate())
     }
 
     @Test
-    fun invalidMoodEntry_FailsValidation() {
+    fun blankMoodLabel_ReturnsFalse() {
         val mood = MoodEntryModel(
-            moodColor = 0,
+            moodColor = 123,
             moodLabel = "",
-            notes = "",
-            date = LocalDate.now()
+            lat = 52.0,
+            lng = -7.0
         )
+
         assertFalse(mood.validate())
     }
 
     @Test
-    fun moodEntry_DefaultDate_IsToday() {
+    fun zeroMoodColor_ReturnsFalse() {
+        val mood = MoodEntryModel(
+            moodColor = 0,
+            moodLabel = "Sad",
+            lat = 52.0,
+            lng = -7.0
+        )
+
+        assertFalse(mood.validate())
+    }
+
+    @Test
+    fun zeroLatitude_ReturnsFalse() {
         val mood = MoodEntryModel(
             moodColor = 123,
             moodLabel = "Calm",
-            notes = "Just chilling"
+            lat = 0.0,
+            lng = -7.0
         )
-        assertEquals(LocalDate.now(), mood.date)
+
+        assertFalse(mood.validate())
+    }
+
+    @Test
+    fun zeroLongitude_ReturnsFalse() {
+        val mood = MoodEntryModel(
+            moodColor = 123,
+            moodLabel = "Angry",
+            lat = 52.0,
+            lng = 0.0
+        )
+
+        assertFalse(mood.validate())
+    }
+
+    @Test
+    fun notesAreOptional_ValidationStillPasses() {
+        val mood = MoodEntryModel(
+            moodColor = 123,
+            moodLabel = "Neutral",
+            notes = "",
+            lat = 52.0,
+            lng = -7.0
+        )
+
+        assertTrue(mood.validate())
     }
 }
